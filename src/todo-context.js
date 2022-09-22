98c932c7417af43e5ece42f947efba8b51f51871
Magdalena Perry <magperry16@gmail.com>
2022-09-22 18:59:41 -0400

    error fixed

--- src/todo-context.js
+++ src/todo-context.js
@@ -1,16 +1,47 @@
 import React, { createContext } from "react";
 
-import { todosTemplate } from "./app";
-
 export const TodosContext = createContext();
 
+const todosTemplate = [
+  {
+    id: 0,
+    label: "Fix an ability to display all tasks",
+    checked: false,
+  },
+  {
+    id: 1,
+    label: "Fix a layout, checkboxes should be listed in a column",
+    checked: false,
+  },
+  {
+    id: 2,
+    label: "Fix an ability to add a new task",
+    checked: false,
+  },
+  {
+    id: 3,
+    label: "Fix an ability to toggle a task",
+    checked: false,
+  },
+  {
+    id: 4,
+    label: "Fix an ability to delete a task",
+    checked: false,
+  },
+  {
+    id: 5,
+    label: "Fix an ability to count completed tasks",
+    checked: false,
+  },
+];
+
 const TodoContextProvider = (props) => {
-  const [todos, setTodos] = React.useState((todosTemplate) => {
+  const [todos, setTodos] = React.useState(() => {
     const lsTodos = localStorage.getItem("todos");
-    console.log(lsTodos);
+    // console.log(lsTodos);
     if (!lsTodos) {
       return todosTemplate;
-    } else {
+    } else if (lsTodos) {
       return JSON.parse(lsTodos);
     }
   });
